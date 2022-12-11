cb175f15f095ca32d989e807c5923919da41d94c
niko.brusselaers <niko.brusselaers@student.ehb.be>
2022-12-11 22:19:13 +0100

    created a function for getting a random game
    created a anonymous function for when the user presses the getRandomGame button, to make a fetch call and display the randomly selected game on the page

---
+++ src/index.js
@@ -0,0 +1,53 @@
+/**
+ * 
+ *      used sources:
+ * 
+ *              -enable cors: https: //developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+ *              -css: https: //www.w3schools.com/css/default.asp
+ */
+
+
+const findOnPreferenceBtn = document.getElementById("findOnPreferenceBtn")
+const findRandomBtn = document.getElementById("findRandomBtn")
+const findFromFavoritesBtn = document.getElementById("findFromFavoritesBtn")
+
+
+const url = 'https://gamepickr-backend.onrender.com'
+findRandomBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    console.log('click');
+    fetch(`${url}/getRandomGame`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            displayGame(data)
+        })
+})
+
+const displayGame = (gameData) => {
+    let innerHTML = `
+            <h1> random game </h1> 
+            <p> We have selected a random game for you. <br>
+                Not happy ? <br>
+                you can re - roll by clicking retry </p>
+            <div class = "gameCard"id = "gameCard">
+                <img src = "${gameData.image}"alt = "">
+                <div>
+                    <button id = "dislikeBtn"> <span class = "icon-thumbs-down"> </span></button>
+                    <h3> ${gameData.name} </h3> 
+                    <button id = "likeBtn" > <span class = "icon-heart"> </button>
+                    <h4 class = "gameId" id = "gameId"> ${gameData.gameId} </h4> 
+                </div> 
+            </div> 
+            <button id = "retryBtn">
+                <p> retry </p>
+                <span class="icon-loop2"></span>
+            </button> 
+        `
+    document.getElementById('mainPageContainer').innerHTML = innerHTML
+}
