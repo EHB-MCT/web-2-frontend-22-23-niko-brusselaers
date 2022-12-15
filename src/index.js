61694d828d394fe335cebbb521d53e3677c9b596
niko.brusselaers <niko.brusselaers@student.ehb.be>
2022-12-15 14:22:26 +0100

    moved used sources to readme file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-/**
- * 
- *      used sources:
- * 
- *              - WEB II frontend niko brusselaers: https: //github.com/EHB-MCT/web2-frontend-niko-brusselaers
- *              - enable cors: https: //developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
- *              - css: https: //www.w3schools.com/css/default.asp
- *              - exporting function: https: //developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
- *              - how to use localstorage: https: //developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
- *              - how to use sessionstorage: https: //developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
- *              - how to add a class to an element javascript: https: //www.w3schools.com/howto/howto_js_add_class.asp
- *              - how to change input placeholder text https: //stackoverflow.com/questions/13506481/change-placeholder-text
- * 
- * 
- */
+
 
 const isLoggedIn = sessionStorage.getItem('isLoggedIn')
 console.log(isLoggedIn);
