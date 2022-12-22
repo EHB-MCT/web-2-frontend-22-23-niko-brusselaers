e6eda738fbaeb7858c6168ddbd147a6b890cea3b
niko.brusselaers <niko.brusselaers@student.ehb.be>
2022-12-22 14:32:31 +0100

    added an event listener to buttons of randomly selected game
    added an event listener to buttons of randomly selected game that perform a fetch call and update userGamePrefrence in database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-
-
 const isLoggedIn = sessionStorage.getItem('isLoggedIn')
-console.log(isLoggedIn);
-const url = 'https://gamepickr-backend.onrender.com'
+// var URL = 'https://gamepickr-backend.onrender.com'
+var URL = "http://localhost:3000"
 document.getElementById('index').classList.add("activeLink")
 
 //if user is logged in or not, display a series of buttons for selecting a random game
@@ -13,7 +11,7 @@ displayMainpageButtons(isLoggedIn)
 const findRandomBtn = document.getElementById("findRandomBtn")
 findRandomBtn.addEventListener("click", (event) => {
     event.preventDefault()
-    fetch(`${url}/getRandomGame`, {
+    fetch(`${URL}/getRandomGame`, {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
@@ -36,7 +34,7 @@ if (isLoggedIn == "true") {
     findFromFavoritesBtn.addEventListener('click', () => {
         let userId = sessionStorage.getItem('id') || localStorage.getItem('id')
         console.log(userId);
-        fetch(`${url}/getGameFromFavorites`, {
+        fetch(`${URL}/getGameFromFavorites`, {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json',
@@ -62,17 +60,44 @@ function displayGame(gameData) {
             <h1> Random Game </h1> 
             <p> We have selected a random game for you. <br>
                  </p>
-            <div class = "gameCard"id = "gameCard">
-                <img src = "${gameData.image}"alt = "">
+            <div class="gameCard" id="gameCard">
+                <img src="${gameData.image}" alt="">
                 <div>
-                    <button id = "dislikeBtn"> <span class = "icon-thumbs-down"> </span></button>
-                    <h3> ${gameData.name} </h3> 
-                    <button id = "likeBtn" > <span class = "icon-heart"> </button>
-                    <h4 class = "gameId" id = "gameId"> ${gameData.gameId} </h4> 
+                    <button id="dislikeBtn"> <span class="icon-thumbs-down" id="dislikeBtn"> </span></button>
+                    <h3>${gameData.name}</h3> 
+                    <button id="likeBtn" > <span class = "icon-heart" id="likeBtn"> </button>
+                    <h4 class="gameId" id="gameId">${gameData.gameId}</h4> 
                 </div> 
             </div> 
         `
     document.getElementById('mainContent').innerHTML = innerHTML
+    const gameCard = document.querySelector('#gameCard')
+    gameCard.addEventListener('click', async (element) => {
+        //if clicked element is not gameCard, retrieve userId and gameId
+        if (element.target != element.currentTarget) {
+            let updateUserPreferences = {
+                userId: sessionStorage.getItem('id') || localStorage.getItem('id'),
+                gameId: element.currentTarget.querySelector('h4').innerHTML,
+                isLiked: null
+            }
+            // if likedBtn has been clicked set isLiked true else if dislikeBtn has been clicked set it to false
+            if (element.target.id == "likeBtn") updateUserPreferences.isLiked = true
+            else if (element.target.id == "dislikeBtn") updateUserPreferences.isLiked = false
+            // do a fetch call to update User Game Preference with the like/dislike of the displayed game
+            let statusCode
+            await fetch(`${URL}/updateUserGamePreference`, {
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    updateUserPreferences
+                })
+            })
+
+
+        }
+    })
 }
 
 
@@ -99,6 +124,6 @@ function displayMainpageButtons(isLoggedIn) {
     mainPageBtnContainer.innerHTML = innerHTML
 }
 
-function displayQuuestions() {
+function displayQuestions() {
 
 }
